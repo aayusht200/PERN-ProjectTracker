2d56f8b2afb1c3c65dbaaa6aee9056d47a39d5ba
Aayush Trivedi <aayush.t200@gmail.com>
2026-06-15 15:03:54 +0530

    feat(database): add sample project and task data to dummyProject.sql feat(package): update dependencies to include dotenv and pg

--- server/db/dummyProject.sql
+++ server/db/dummyProject.sql
@@ -16,3 +16,15 @@ INSERT INTO project VALUES(
     '2026-05-28',
     '2026-06-10'
 );
+
+
+INSERT INTO project_task VALUES(
+    '95e009e5-0a8b-490b-836a-973a0af16d11',
+    'ebf9b4ca-702b-420b-98ad-8484e5d1899c',
+    'Clock Component',
+    'Create a clock component with current time & date.',
+    'open',
+    '2026-05-28',
+    '2026-06-10'
+);
+-- DELETE FROM project;
