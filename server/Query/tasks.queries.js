b78e6f3149b42771ebb9c73654efb55fb8ebb7db
Aayush Trivedi <aayush.t200@gmail.com>
2026-06-16 10:49:52 +0530

    feat(server): update task queries and implement CRUD operations for tasks

--- server/Query/tasks.queries.js
+++ server/Query/tasks.queries.js
@@ -1,8 +1,8 @@
 export const tasks = {
     selectAll: `SELECT * FROM project_task WHERE project_id=$1; `,
     selectOne: `SELECT * FROM project_task WHERE project_id=$1 AND id=$2;`,
-    delete: `DELETE FROM project_task WHERE id=$1`,
-    edit: `UPDATE project_task SET title=$1,description=$2,status=$3,start_date=$4,end_date=$5,updated_at=NOW() WHERE id=$6;`,
+    delete: `DELETE FROM project_task WHERE project_id=$1 AND id=$2;`,
+    edit: `UPDATE project_task SET title=$1,description=$2,status=$3,start_date=$4,end_date=$5,updated_at=NOW() WHERE project_id=$6 AND id=$7;`,
     createNew: `INSERT INTO project_task (
       project_id,
       title,
--- server/controllers/tasks.routes.js
+++ server/controllers/tasks.routes.js
@@ -1,3 +1,4 @@
+import { response } from 'express';
 import { pool } from '../db.js';
 import { tasks } from '../Query/tasks.queries.js';
 export const getProjectTasks = (req, res) => {
@@ -13,8 +14,61 @@ export const getProjectTasks = (req, res) => {
         });
 };
 export const getTask = (req, res) => {
-    const { projectId, taskId } = res.params;
+    const { projectId, taskId } = req.params;
+    const values = [projectId, taskId];
+    pool.query(tasks.selectOne, values)
+        .then((response) => {
+            res.send(response.rows);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(500).send({ message: 'Internal Server Error' });
+        });
 };
-export const createTask = (req, res) => {};
-export const editTask = (req, res) => {};
-export const deleteTask = (req, res) => {};
+export const createTask = (req, res) => {
+    const { projectId } = req.params;
+    const { title, description, status, start_date, end_date } = req.body;
+    const values = [projectId, title, description, status, start_date, end_date];
+    pool.query(tasks.createNew, values)
+        .then((response) => {
+            if (response.rowCount === 1) res.status(201).send({ message: 'insertion successful' });
+            else {
+                res.status(400).send({ message: 'insertion failed' });
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(500).send({ message: 'Internal Server Error' });
+        });
+};
+export const editTask = (req, res) => {
+    const { projectId, taskId } = req.params;
+    const { title, description, status, start_date, end_date } = req.body;
+    const values = [title, description, status, start_date, end_date, projectId, taskId];
+    pool.query(tasks.edit, values)
+        .then((response) => {
+            if (response.rowCount === 1) res.send({ message: 'task update successful' });
+            else {
+                res.status(400).send({ message: 'taskId/projectId does not exist' });
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(500).send({ message: 'Internal Server Error' });
+        });
+};
+export const deleteTask = (req, res) => {
+    const { projectId, taskId } = req.params;
+    const values = [projectId, taskId];
+    pool.query(tasks.delete, values)
+        .then((response) => {
+            if (response.rowCount === 1) res.send({ message: 'task delete successful' });
+            else {
+                res.status(400).send({ message: 'taskId/projectId does not exist' });
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(500).send({ message: 'Internal Server Error' });
+        });
+};
